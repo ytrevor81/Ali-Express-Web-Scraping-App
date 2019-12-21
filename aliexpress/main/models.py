a077cb27329282a5caaf016762b7ab5d87585f91
Trevor Youngquist <46886041+ytrevor81@users.noreply.github.com>
2019-12-21 23:25:22 +0700

    a fantastic start ;)

--- aliexpress/main/models.py
+++ aliexpress/main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class AliSubmission(models.Model):
+    product = models.CharField(max_length=250)
+    price = models.FloatField()
+    rating = models.FloatField()
+    sold = models.IntegerField()
+    epacket = models.BooleanField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.product
--- aliexpress/main/views.py
+++ aliexpress/main/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from .models import *
 
-# Create your views here.
+def homepage(request):
+    return render(request, "index.html", {})
